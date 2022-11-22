5fd118f2f4c11f23f7c98341ca4ceb593fc6164f
Vu <tranngocvu4220@gmail.com>
2022-11-22 16:46:52 +0700

    Login function

--- assets/js/login.js
+++ assets/js/login.js
@@ -32,3 +32,72 @@ function changetoSingin() {
     document.getElementById("login-up").style.display="none";
     document.getElementById("login-in").style.display="block";
 }
+
+
+function admin(){
+    var userArray=[];
+    if(localStorage.getItem('user')==null){
+        var user1={
+            username:"admin",
+            email:"admin@gmail.com",
+            password:"admin",
+            usertype:1
+            }
+            userArray.push(user1);
+            localStorage.setItem('user',JSON.stringify(userArray));
+    }
+    
+}
+
+function signUp(){
+    userArray = JSON.parse(localStorage.getItem('user'));
+
+    var username = document.getElementById("username__dk").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password__dk").value;
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if(!username.replace(/\s+/,'').length){
+        alert("User rong");
+        return false;
+    }else if(!email.match(mailformat)){
+        alert('Input is not valid type');
+        return false
+     }
+    if(!password.replace(/\s+/,'').length){
+        alert("Password rong");
+        return false;
+    }
+
+
+    let user={
+        username:username,
+        email:email,
+        password:password,
+        usertype:0
+    }
+    
+    userArray.push(user);
+    localStorage.setItem('user',JSON.stringify(userArray));
+}
+
+function signIn(){
+    var username = document.getElementById("username__dn").value;
+    var password = document.getElementById("password__dn").value;
+    
+	var userArray = JSON.parse(localStorage.getItem('user'));
+    for(i=0;i<userArray.length;i++)
+		if(userArray[i].username==username && userArray[i].password==password){
+			alert("Dang nhap thanh cong");
+            hideall();
+            break;
+        }
+	    else{
+        alert("dang nhap khong thanh cong");
+        break;
+        }
+}
+
+window.onload=function(){
+    admin();
+}
